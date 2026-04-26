914272403a52ffd3ebee073469e81991ab2ed3ad
Kauã Santana <kauassilva0023@gmail.com>
2026-04-26 13:50:40 -0300

    passe o acesso à playlist para um .py e adiciona playlist.json

---
+++ access.py
@@ -0,0 +1,32 @@
+# libs
+from auth import auth
+import json
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# autenticação
+CLIENT_ID, CLIENT_SECRET, REDIRECT_URI = auth()
+SCOPE = 'playlist-read-private'
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id = CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE
+    )
+)
+
+# usuario (eu mesmo)
+user    = sp.current_user()
+user_id = user['id']
+
+# salvando playlist
+playlist_id = 'spotify:playlist:1rWIhl1hpQnpkUDRV4RIKT'
+playlist = sp.playlist_tracks(
+    playlist_id=playlist_id,
+)
+
+with open("playlist.json", "w") as outfile:
+    json.dump(playlist['items'], outfile)
